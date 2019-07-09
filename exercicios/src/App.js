263b59ebd91a9a3e9da8903ccfadd3f64e7d45ad
Vagner Silva Oliveira <vagner@localhost.localdomain>
2019-07-08 22:45:15 -0300

    first commit

---
+++ exercicios/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react"
+import { View, StyleSheet } from 'react-native'
+import Simples from "./componentes/Simples";
+
+export default class App extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Simples texto="Flexível!!!"/>  
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
