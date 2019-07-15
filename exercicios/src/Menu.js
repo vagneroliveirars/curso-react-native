1deafa0b29d556e29ac4c783825a88dab32f4535
vagner_soliveira <vagner_soliveira@terceiros.sicredi.com.br>
2019-07-15 19:06:00 -0300

    Aula 29 - Eventos e componentes controlados

--- exercicios/src/Menu.js
+++ exercicios/src/Menu.js
@@ -7,8 +7,12 @@ import { Inverter, MegaSena } from './componentes/Multi'
 import Contador from './componentes/Contador'
 import Plataformas from './componentes/Plataformas'
 import ValidarProps from './componentes/ValidarProps'
+import Evento from './componentes/Evento'
 
 const MainNavigator = createDrawerNavigator({
+    Evento: {
+        screen: Evento
+    },
     ValidarProps: {
         screen: () => <ValidarProps label="Teste: " ano={19}/>
     },
