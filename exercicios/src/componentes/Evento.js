43e0e6647869f0de8e0c19ecc57c56449175b4b6
Vagner Silva Oliveira <vagner.oliveirars@gmail.com>
2019-07-15 20:41:14 -0300

    Aula 29 - Eventos e componentes controlados

--- exercicios/src/componentes/Evento.js
+++ exercicios/src/componentes/Evento.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react'
 import { View, Text, TextInput } from 'react-native'
 import Padrao from '../estilo/Padrao'
 
+/**
+ * Componente controlado refletindo o state.
+ * Neste exemplo, é capturado um evento onChangeText no TextInput que chama a
+ * função alterarTexto passando o texto do input e atualizando a propriedade texto
+ * do state. Após alterar o texto, o componente Text é automaticamente atualizado.
+ * Para criar um componente não controlado, uma das estratégias é inicializar a 
+ * propriedade do state com null. Exemplo:
+ * state = { texto : null }
+ */
 export default class Evento extends Component {
 
     state = {
