a3a00e6c36b5ed1321456859df64a5e4e03aa479
vagner_soliveira <vagner_soliveira@terceiros.sicredi.com.br>
2019-07-15 18:52:52 -0300

    Aula 28 - Validar propriedades

--- exercicios/src/Menu.js
+++ exercicios/src/Menu.js
@@ -1,54 +1,17 @@
 import React from 'react'
-import { createStackNavigator, createAppContainer, createDrawerNavigator } from 'react-navigation';
+import { createAppContainer, createDrawerNavigator } from 'react-navigation';
 
 import Simples from './componentes/Simples'
 import ParImpar from './componentes/ParImpar'
 import { Inverter, MegaSena } from './componentes/Multi'
 import Contador from './componentes/Contador'
 import Plataformas from './componentes/Plataformas'
-
-/* export default createDrawerNavigator({
-    MegaSena: {
-        // É necessário usar arrow function para passar parâmetros para o componente
-        screen: () => <MegaSena numeros={8}/>,
-        navigationOptions: { title: 'Mega Sena' }
-    },
-    Inverter: {
-        screen: () => <Inverter texto='React Native!'/>
-    },
-    ParImpar: {
-        screen: () => <ParImpar numero={30}/>,
-        navigationOptions: { title: 'Par & Ímpar'}
-    },
-    Simples: {
-        screen: () => <Simples texto='Flexível'/>
-    }
-
-}, { drawerWidth: 300 }) */
-
-/* const MainNavigator = createStackNavigator({
-    MegaSena: {
-        // É necessário usar arrow function para passar parâmetros para o componente
-        screen: () => <MegaSena numeros={8} />,
-        navigationOptions: { title: 'Mega Sena' }
-    },
-    Inverter: {
-        screen: () => <Inverter texto='React Native!' />
-    },
-    ParImpar: {
-        screen: () => <ParImpar numero={30} />,
-        navigationOptions: { title: 'Par & Ímpar' }
-    },
-    Simples: {
-        screen: () => <Simples texto='Flexível' />
-    }
-});
-
-const App = createAppContainer(MainNavigator);
-
-export default App; */
+import ValidarProps from './componentes/ValidarProps'
 
 const MainNavigator = createDrawerNavigator({
+    ValidarProps: {
+        screen: () => <ValidarProps label="Teste: " ano={19}/>
+    },
     Plataformas: {
         screen: Plataformas
     },
