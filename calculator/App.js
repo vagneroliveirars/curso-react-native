ddcfbb34281c15a858aeb3ec33e7f55e44695ecc
vagner_soliveira <vagner_soliveira@terceiros.sicredi.com.br>
2019-07-25 19:04:57 -0300

    Aula 41 - Componente botão

--- calculator/App.js
+++ calculator/App.js
@@ -9,29 +9,37 @@ export default class App extends Component {
     displayValue: '0'
   }
 
+  addDigit = n => this.setState({ displayValue: n })
+
+  clearMemory = () => this.setState({ displayValue: '0' })
+
+  setOperation = operation => {
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Display value={this.state.displayValue}/>
-        
-        <View style={styles.button}>
-          <Button label='AC'/>
-          <Button label='/'/>
-          <Button label='7'/>
-          <Button label='8'/>
-          <Button label='9'/>
-          <Button label='*'/>
-          <Button label='4'/>
-          <Button label='5'/>
-          <Button label='6'/>
-          <Button label='-'/>
-          <Button label='1'/>
-          <Button label='2'/>
-          <Button label='3'/>
-          <Button label='+'/>
-          <Button label='0'/>
-          <Button label='.'/>
-          <Button label='='/>
+
+        <View style={styles.buttons}>
+          <Button label='AC' triple onClick={this.clearMemory}/>
+          <Button label='/' operation onClick={this.setOperation}/>
+          <Button label='7' onClick={this.addDigit}/>
+          <Button label='8' onClick={this.addDigit}/>
+          <Button label='9' onClick={this.addDigit}/>
+          <Button label='*' operation onClick={this.setOperation}/>
+          <Button label='4' onClick={this.addDigit}/>
+          <Button label='5' onClick={this.addDigit}/>
+          <Button label='6' onClick={this.addDigit}/>
+          <Button label='-' operation onClick={this.setOperation}/>
+          <Button label='1' onClick={this.addDigit}/>
+          <Button label='2' onClick={this.addDigit}/>
+          <Button label='3' onClick={this.addDigit}/>
+          <Button label='+' operation onClick={this.setOperation}/>
+          <Button label='0' double onClick={this.addDigit}/>
+          <Button label='.' onClick={this.addDigit}/>
+          <Button label='=' operation onClick={this.setOperation}/>
         </View>
       </View>
     )
@@ -42,7 +50,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   },
-  button: {
+  buttons: {
     flexDirection: 'row',
     // wrap permite quebra na linha dos componentes
     flexWrap: 'wrap'
