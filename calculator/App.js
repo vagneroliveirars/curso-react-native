757545220ae9eefcaeb0e5b7f1bc105f348ec191
vagner_soliveira <vagner_soliveira@terceiros.sicredi.com.br>
2019-07-26 18:45:51 -0300

    Aula 44 - Lógica da calculadora

--- calculator/App.js
+++ calculator/App.js
@@ -3,18 +3,70 @@ import { StyleSheet, View, Text } from 'react-native'
 import Button from './src/components/Button';
 import Display from './src/components/Display';
 
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0
+}
+
 export default class App extends Component {
 
-  state = {
-    displayValue: '0'
+  state = { ...initialState }
+  
+  addDigit = n => {
+    // Ignora mais de um ponto
+    if (n === '.' && this.state.displayValue.includes('.')) {
+      return
+    }
+    
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({ displayValue, clearDisplay: false })
+
+    // Valida se foi digitado um valor válido
+    if (n !== '.') {
+      const newValue = parseFloat(displayValue)
+      const values = [ ...this.state.values ]
+      values[this.state.current] = newValue
+      this.setState({ values })
+    }
+
   }
 
-  addDigit = n => this.setState({ displayValue: n })
-
-  clearMemory = () => this.setState({ displayValue: '0' })
+  // Restaura o estado inicial da calculadora usando o operador spread
+  clearMemory = () => this.setState({ ...initialState })
 
   setOperation = operation => {
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true })
+    } else {
+      const equals = operation === '='
+      const values = [ ...this.state.values ]
 
+      try {
+        /**
+         * Interpolações com eval para para avaliar a sentença.
+         * Try/catch para o caso de fazer alguma atribuição incorreta
+         */
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      } catch (e) {
+        // Recebe zero em caso de erro
+        values[0] = this.state.values[0]
+      }
+
+      values[1] = 0
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        //clearDisplay: !equals,
+        clearDisplay: true,
+        values
+      })
+    }
   }
 
   render() {
