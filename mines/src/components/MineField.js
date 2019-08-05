7a09777e5e37901a0337fa638653d10aec88d5eb
vagner_soliveira <vagner_soliveira@terceiros.sicredi.com.br>
2019-08-05 18:14:13 -0300

    Aula 57 - Correção no componente MineField para mostrar os Fields na mesma linha

--- mines/src/components/MineField.js
+++ mines/src/components/MineField.js
@@ -17,14 +17,13 @@ export default props => {
              */ 
             return <Field { ...field} key={c}/>
         })
-        return <View key={r}>{columns}</View>
+        return <View style={{flexDirection: 'row'}} key={r}>{columns}</View>
     })
     return <View style={styles.container}>{rows}</View>
 }
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'row',
         backgroundColor: '#EEE'
     }
 })
