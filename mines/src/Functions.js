1acd77120a5a9fd45b91c6a844f4cc0d0401810f
vagner_soliveira <vagner_soliveira@terceiros.sicredi.com.br>
2019-08-05 19:27:40 -0300

    Aula 59 - Lógica do jogo #02

--- mines/src/Functions.js
+++ mines/src/Functions.js
@@ -66,4 +66,97 @@ const createMinedBoard = (rows, columns, minesAmount) => {
     return board
 }
 
-export { createMinedBoard }
+const cloneBoard = board => {
+    return board.map(rows => {
+        return rows.map(field => {
+            return { ...field }
+        })
+    })
+}
+
+/**
+ * Retorna os vizinhos ao redor de um field
+ * 
+ * @param {*} board 
+ * @param {*} row 
+ * @param {*} column 
+ */
+const getNeighbors = (board, row, column) => {
+    const neighbors = []
+    const rows = [row - 1, row, row + 1]
+    const colums = [column - 1, column, column + 1]
+
+    rows.forEach(r => {
+        columns.forEach(c => {
+            const different = r != row || c != column
+            const validRow = r >= 0 && r < board.length
+            const validColumn = r >= 0 && c < board[0].length
+            if (different && validRow && validColumn) {
+                neighbors.push(board[r][c])
+            }
+        })
+    })
+
+    return neighbors
+}
+
+/**
+ * Retorna se a vizinhança ao redor de um field é segura
+ * 
+ * @param {*} board 
+ * @param {*} row 
+ * @param {*} column 
+ */
+const safeNeighborhood = (board, row, column) => {
+    // Função usando a lógica do reduce: 
+    // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+    const safes = (result, neighbor) => result && !neighbor.mined
+    return getNeighbors(board, row, column).reduce(safes, true)
+}
+
+const openField = (board, row, column) => {
+    const field = board[row][column]
+    if (!field.open) {
+        field.open = true
+        if (field.mined) {
+            field.exploded = true
+        } else {
+            const neighbors = getNeighbors(board, row, column)
+            if (safeNeighborhood(board, row, column)) {
+                // Se a vizinhança for segura, abre os vizinhos ao redor recursivamente
+                neighbors(board, row, column)
+                    .forEach(neighbor => openField(board, neighbor.row, neighbor.column))
+            } else {
+                // Se a vizinhança não é segura, conta quantas minhas tem ao redor do campo
+                field.nearMines = neighbors.filter(neighbor => neighbor.mined).length
+            }
+        }
+    }
+}
+
+// Retorna um array com todos os fields do board
+const fields = board => [].concat(...board)
+
+// Retorna se algum field do board explodiu
+const hasExplosion = board => fields(board).filter(field => field.exploded).length > 0
+
+// Retorna se o campo está pendente
+const pendding = field => (field.mined && !field.flagged) || (!field.mined && !field.open)
+
+// Retorna se o usuário ganhou o jogo quando nenhum dos campos está pendente
+const wonGame = board => fields(board).filter(pendding).length === 0
+
+// Mostra todos os campos minados
+const showMines = board => 
+    fields(board)
+    .filter(field => field.mined)
+    .forEach(field => field.open = true)
+
+export { 
+    createMinedBoard,
+    cloneBoard,
+    openField,
+    hasExplosion,
+    wonGame,
+    showMines
+}
