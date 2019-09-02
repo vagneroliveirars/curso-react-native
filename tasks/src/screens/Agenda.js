3ccd5fef81030c91ec1e36e146fbdfb4393358b0
vagner_soliveira <vagner_soliveira@terceiros.sicredi.com.br>
2019-09-02 19:18:36 -0300

    Aula 70 - Filtrar tarefas (tasks)

--- tasks/src/screens/Agenda.js
+++ tasks/src/screens/Agenda.js
@@ -4,8 +4,11 @@ import {
     Text,
     View,
     ImageBackground,
-    FlatList
+    FlatList,
+    TouchableOpacity,
+    Platform
 } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome'
 import moment from 'moment'
 /**
  * Este import precisa ser carregado para o formato de datas do Brasil,
@@ -31,7 +34,30 @@ export default class Agenda extends Component {
             { id: Math.random(), desc: 'Concluir o curso', estimateAt: new Date() },
             { id: Math.random(), desc: 'Comprar o curso de React Native', estimateAt: new Date(), doneAt: new Date() },
             { id: Math.random(), desc: 'Concluir o curso', estimateAt: new Date() }
-        ]
+        ],
+        visibleTasks: [],
+        showDoneTasks: true
+    }
+
+    filterTasks = () => {
+        let visibleTasks = null;
+        if (this.state.showDoneTasks) {
+            visibleTasks = [...this.state.tasks]
+        } else {
+            const pending = task => !task.doneAt
+            visibleTasks = this.state.tasks.filter(pending)
+        }
+
+        this.setState({ visibleTasks })
+    }
+
+    toogleFilter = () => {
+        /** 
+         * Alterna showDoneTasks para true ou false e chama a função de callback
+         * filterTasks depois que o estado da aplicação for alterado, pois a função
+         * this.setState é assíncrona
+         * */ 
+        this.setState({ showDoneTasks: !this.state.showDoneTasks}, this.filterTasks)
     }
 
     toogleTask = id => {
@@ -43,13 +69,27 @@ export default class Agenda extends Component {
                 return task
             })
 
-        this.setState({ tasks })
+        this.setState({ tasks }, this.filterTasks)
+    }
+
+    /**
+     * Método do ciclo de vida do React.
+     * Called immediately after a component is mounted. Setting state here will trigger re-rendering.
+     */
+    componentDidMount = () => {
+        this.filterTasks()
     }
 
     render() {
         return (
             <View style={styles.container}>
                 <ImageBackground source={todayImage} style={styles.background}>
+                    <View style={styles.iconBar}>
+                        <TouchableOpacity onPress={this.toogleFilter}>
+                            <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'} 
+                                size={20} color={CommonStyles.colors.secondary}/>
+                        </TouchableOpacity>
+                    </View>
                     <View style={styles.titleBar}>
                         <Text style={styles.title}>Hoje</Text>
                         <Text style={styles.subtitle}>
@@ -58,7 +98,7 @@ export default class Agenda extends Component {
                     </View>
                 </ImageBackground>
                 <View style={styles.taskContainer}>
-                    <FlatList data={this.state.tasks}
+                    <FlatList data={this.state.visibleTasks}
                         keyExtractor={item => `${item.id}`}
                         renderItem={({ item }) => <Task {...item} toogleTask={this.toogleTask}/>} />
                 </View>
@@ -94,5 +134,11 @@ const styles = StyleSheet.create({
     },
     taskContainer: {
         flex: 7,
+    },
+    iconBar: {
+        marginTop: Platform.OS === 'ios' ? 30 : 20,
+        marginHorizontal: 20,
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
     }
 })
